4b21b58ec91e64d8c0a0478d2f6f453be4b4c41a
hxrxchang <m.y.y.0326.cg@gmail.com>
2019-04-11 09:59:36 +0900

    settings for vscode

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,4 +1,5 @@
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
+ln -sf ~/dotfiles/vscode/settings.json ~/Library/Application\ Support//Code/User/settings.json
 
