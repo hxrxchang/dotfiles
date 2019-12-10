98c5ec4c9ffa839e3a71d8f6b5064bedd0521e44
hxrxchang <m.y.y.0326.cg@gmail.com>
2019-12-10 15:11:19 +0900

    spectacle setting ref: https://github.com/eczarny/spectacle/issues/244

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -2,4 +2,4 @@ ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
 ln -sf ~/dotfiles/vscode/settings.json ~/Library/Application\ Support//Code/User/settings.json
-
+ln -sf ~/dotfiles/spectacle/Shortcuts.json ~/Library/Application\ Support/Spectacle/Shortcuts.json
