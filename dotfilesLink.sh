1664dff0797c538c66066b4fdd898325f5a684ae
hxrxchang <m.y.y.0326.cg@gmail.com>
2020-07-04 00:13:05 +0900

    fix path about git files

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,7 +1,7 @@
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
-ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
-ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
-ln -sf ~/dotfiles/.gitconfig.local ~/.gitconfig.local
+ln -sf ~/dotfiles/git/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/git/.gitignore_global ~/.gitignore_global
+ln -sf ~/dotfiles/git/.gitconfig.local ~/.gitconfig.local
+ln -sf ~/dotfiles/git/.git-prompt.sh ~/.git-prompt.sh
 ln -sf ~/dotfiles/vscode/settings.json ~/Library/Application\ Support//Code/User/settings.json
 ln -sf ~/dotfiles/spectacle/Shortcuts.json ~/Library/Application\ Support/Spectacle/Shortcuts.json
-ln -sf ~/dotfiles/.git-prompt.sh ~/.git-prompt.sh
