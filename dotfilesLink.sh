c541c5ce4c2174d7ddbe36120b31664870218ab8
hxrxchang <m.y.y.0326.cg@gmail.com>
2020-07-04 00:21:49 +0900

    shell -> Makefile

--- dotfilesLink.sh
+++
@@ -1,7 +0,0 @@
-ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
-ln -sf ~/dotfiles/git/.gitconfig ~/.gitconfig
-ln -sf ~/dotfiles/git/.gitignore_global ~/.gitignore_global
-ln -sf ~/dotfiles/git/.gitconfig.local ~/.gitconfig.local
-ln -sf ~/dotfiles/git/.git-prompt.sh ~/.git-prompt.sh
-ln -sf ~/dotfiles/vscode/settings.json ~/Library/Application\ Support//Code/User/settings.json
-ln -sf ~/dotfiles/spectacle/Shortcuts.json ~/Library/Application\ Support/Spectacle/Shortcuts.json
