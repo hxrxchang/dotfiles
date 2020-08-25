6cbee7e9abf645b451560d5a16ffd26b5ab80ee3
hxrxchang <m.y.y.0326.cg@gmail.com>
2020-08-25 17:53:54 +0900

    .git-complehention.bashで補完

--- git/.git-prompt.sh
+++ git/.git-prompt.sh
@@ -562,18 +562,3 @@ __git_ps1 ()
 
 	return $exit
 }
-
-# Setup completion for certain functions defined above by setting common
-# variables and workarounds.
-# This is NOT a public function; use at your own risk.
-__git_complete ()
-{
-    local wrapper="__git_wrap${2}"
-    eval "$wrapper () { __git_func_wrap $2 ; }"
-    complete -o bashdefault -o default -o nospace -F $wrapper $1 2>/dev/null \
-        || complete -o default -o nospace -F $wrapper $1
-}
-
-__git_complete git __git_main
-__git_complete gitk __gitk_main
-__git_complete g __git_main
