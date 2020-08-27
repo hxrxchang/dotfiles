c64a1fe6c57f236f168813b48040bd9395a756c5
hxrxchang <m.y.y.0326.cg@gmail.com>
2020-08-28 00:25:28 +0900

    chromeコマンド追加

--- alias.sh
+++ alias.sh
@@ -1,6 +1,7 @@
 alias readbp='source ~/.bash_profile'
 alias checkip='ifconfig | grep "inet " | grep -v 127.0.0.1'
 alias ll='ls -alF'
+alias chrome="open -a '/Applications/Google Chrome.app'"
 
 ## git
 alias g='git'
