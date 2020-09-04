3524e7fe3432936dc50f5ab0788db02268f0e29e
hxrxchang <m.y.y.0326.cg@gmail.com>
2020-09-04 14:47:08 +0900

    gomi

--- alias.sh
+++ alias.sh
@@ -2,6 +2,7 @@ alias readbp='source ~/.bash_profile'
 alias checkip='ifconfig | grep "inet " | grep -v 127.0.0.1'
 alias ll='ls -alF'
 alias chrome="open -a '/Applications/Google Chrome.app'"
+alias rm="gomi"
 
 ## git
 alias g='git'
