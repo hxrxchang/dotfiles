d75f8f9386f0290b8c9fc905f8389d2a0b1978ce
hxrxchang <m.y.y.0326.cg@gmail.com>
2020-09-15 21:11:05 +0900

    vscodeのextensionsを共有

--- alias.sh
+++ alias.sh
@@ -14,3 +14,6 @@ alias gsee='hubrowse'
 
 alias his="peco_search_history"
 alias app="peco_search_app"
+
+alias save-vscode-extensions="code --list-extensions >> ~/dotfiles/vscode/extensions.list"
+alias install-vscode-extensions="cat ~/dotfiles/vscode/extensions.list | xargs -L 1 code --install-extension"
