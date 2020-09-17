4e19d2658e8d1f6b5dd97d794f619412816f493a
hxrxchang <m.y.y.0326.cg@gmail.com>
2020-09-17 13:07:01 +0900

    llコマンドに -h オプションつける

--- alias.sh
+++ alias.sh
@@ -1,7 +1,7 @@
 alias rm="gomi"
 alias sbp='source ~/.bash_profile'
 alias checkip='ifconfig | grep "inet " | grep -v 127.0.0.1'
-alias ll='ls -alF'
+alias ll='ls -alF -h'
 alias chrome="open -a '/Applications/Google Chrome.app'"
 alias slack="open -a '/Applications/Slack.app'"
 
