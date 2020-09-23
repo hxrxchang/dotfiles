94b55d942df4d782048941edb8d34dc3f5617079
hxrxchang <m.y.y.0326.cg@gmail.com>
2020-09-23 22:46:54 +0900

    fix alias

--- alias.sh
+++ alias.sh
@@ -10,7 +10,7 @@ alias g='git'
 alias commit-diff='git diff HEAD~..HEAD'
 alias gsw='git branch | peco | xargs git switch'
 alias cleanbranches='git branch | grep -v "master" | grep -v "*" | xargs git branch -D'
-alias gsee='hubrowse'
+alias gsee='git-remote-opener'
 
 alias his="peco_search_history"
 alias app="peco_search_app"
