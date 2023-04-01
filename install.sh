1071ac73fb03dc0cdaf40626d56f714f700cde8e
hxrxchang <mail@hxrxchang.dev>
2023-04-01 15:19:42 +0900

    install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This script is for GitHub Codespaces.
+ln -sf ./.bash_profile ~/.bash_profile
+ln -sf ./.bashrc ~/.bashrc
+ln -sf ./git/.gitconfig ~/.gitconfig
+ln -sf ./git/.gitignore_global ~/.gitignore_global
+ln -sf ./git/.git-templates ~/.git-templates
+ln -sf ./.vimrc ~/.vimrc
+ln -sf ./atcoder/.atcodertools.toml ~/.atcodertools.toml
