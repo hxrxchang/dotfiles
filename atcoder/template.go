5f318d97efcfcc2a02e8f7b7e70b9a0e8c0974af
hxrxchang <mail@hxrxchang.dev>
2023-04-07 00:46:26 +0900

    update templage.go
    https://zenn.dev/maitake/articles/go_de_atcoder

--- atcoder/template.go
+++ atcoder/template.go
@@ -1,4 +1,126 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+const BUFSIZE = 10000000
+var rdr *bufio.Reader
+
 func main() {
+	rdr = bufio.NewReaderSize(os.Stdin, BUFSIZE)
+	solve()
+}
+
+func solve() {}
+
+// 一行をstringで読み込み
+func readline() string {
+	buf := make([]byte, 0, 16)
+	for {
+		l, p, e := rdr.ReadLine()
+		if e != nil {
+			fmt.Println(e.Error())
+			panic(e)
+		}
+		buf = append(buf, l...)
+		if !p {
+			break
+		}
+	}
+	return string(buf)
+}
+
+// 一行を[]intで読み込み
+func readIntSlice() []int {
+	slice := make([]int, 0)
+	lines := strings.Split(readline(), " ")
+	for _, v := range lines {
+		// s2iはstringをintに変換する関数(後述)
+		slice = append(slice, s2i(v))
+	}
+	return slice
+}
+
+// string <-> int
+func s2i(s string) int {
+	v, ok := strconv.Atoi(s)
+	if ok != nil {
+		panic("Faild : " + s + " can't convert to int")
+	}
+	return v
+}
+
+func i2s(i int) string {
+	return strconv.Itoa(i)
+}
+
+// bool <-> int
+func b2i(b bool) int {
+	if b {
+		return 1
+	}
+	return 0
+}
+
+func i2b(i int) bool {
+  return i != 0
+}
+
+func abs(v int) int {
+	if v < 0 {
+		return -v
+	}
+	return v
+}
+
+func min(values ...int) int {
+	ret := values[0]
+	for _, v := range values {
+		if ret > v {
+			ret = v
+		}
+	}
+	return ret
+}
+
+func max(values ...int) int {
+	ret := values[0]
+	for _, v := range values {
+		if ret < v {
+			ret = v
+		}
+	}
+	return ret
+}
+
+func mod(x, y int) int {
+	m := x % y
+	if m < 0 {
+		return m + y
+	}
+	return m
+}
+
+func pow(x, y int) int {
+	return int(math.Pow(float64(x), float64(y)))
+}
+
+func gcd(v1, v2 int) int {
+	if v1 > v2 {
+		v1, v2 = v2, v1
+	}
+	for v1 != 0 {
+		v1, v2 = v2%v1, v1
+	}
+	return v2
+}
+
+func lcm(v1, v2 int) int {
+	return v1 * v2 / gcd(v1, v2)
 }
