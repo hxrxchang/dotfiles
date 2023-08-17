fedd1fe36c407d7ec38926096626ef83661dda98
hxrxchang <mail@hxrxchang.dev>
2023-08-18 01:57:27 +0900

    add heap

--- atcoder/template.go
+++ atcoder/template.go
@@ -132,3 +132,25 @@ func gcd(v1, v2 int) int {
 func lcm(v1, v2 int) int {
 	return v1 * v2 / gcd(v1, v2)
 }
+
+// heap (priority queue)
+type intHeap []int
+func (h intHeap) Len() int {
+	return len(h)
+}
+func (h intHeap) Less(i, j int) bool {
+	return h[i] < h[j]
+}
+func (h intHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+func (h *intHeap) Push(x interface{}) {
+	*h = append(*h, x.(int))
+}
+func (h *intHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
