a00d7b0ae73e54dfd699dae71e8f9729b4f39fa1
hxrxchang <mail@hxrxchang.dev>
2024-07-20 00:25:31 +0900

    add segment tree

--- atcoder/template.go
+++ atcoder/template.go
@@ -552,6 +552,55 @@ func bisectRight[T constraints.Ordered](slice []T, value T) int {
 	return bisect(slice, func(i int) bool { return slice[i] > value })
 }
 
+// Segment Tree
+type SegmentTree[T any] struct {
+	data []T
+	n    int // 葉の数(全区間の要素数)
+	e    T // 単位元
+	op   func(T, T) T
+}
+
+func NewSegmentTree[T any](n int, e T, op func(T, T) T) *SegmentTree[T] {
+	segtree := &SegmentTree[T]{}
+	segtree.e = e
+	segtree.op = op
+	segtree.n = 1
+	for segtree.n < n {
+		segtree.n *= 2
+	}
+	segtree.data = make([]T, segtree.n*2-1)
+	for i := 0; i < segtree.n*2-1; i++ {
+		segtree.data[i] = segtree.e
+	}
+	return segtree
+}
+
+func (segtree *SegmentTree[T]) Update(idx int, x T) {
+	idx += segtree.n - 1
+	segtree.data[idx] = x
+	for 0 < idx {
+		idx = (idx - 1) / 2
+		segtree.data[idx] = segtree.op(segtree.data[idx*2+1], segtree.data[idx*2+2])
+	}
+}
+
+func (segtree *SegmentTree[T]) query(begin, end, idx, a, b int) T {
+	if b <= begin || end <= a {
+		return segtree.e
+	}
+	if begin <= a && b <= end {
+		return segtree.data[idx]
+	}
+	v1 := segtree.query(begin, end, idx*2+1, a, (a+b)/2)
+	v2 := segtree.query(begin, end, idx*2+2, (a+b)/2, b)
+	return segtree.op(v1, v2)
+}
+
+func (segtree *SegmentTree[T]) Query(begin, end int) T {
+	return segtree.query(begin, end, 0, 0, segtree.n)
+}
+
+
 // sliceを一行で出力
 func printSlice[T any](data []T) {
 	fmt.Println(strings.Trim(fmt.Sprint(data), "[]"))
